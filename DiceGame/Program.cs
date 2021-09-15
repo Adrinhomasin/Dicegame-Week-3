ab0d38512113f8a3f7a2af8988b1fe9cb1108213
Opilane TTHK <opilane@tthk.ee>
2021-09-15 09:42:19 +0300

    randomnumbergenerators

---
+++ DiceGame/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //mängus on kaks mängijat
+            //mõlemad mängijad viskavad täringut
+            //programm kontrollib, kumb mängija viskas rohkem
+            //mängija, kes viskas rohkem ongi võitja
+            //programm kuulutab võitjat
+
+            Random rnd = new Random();
+            int myRandomNumber = rnd.Next(1, 7);
+
+            string PlayerOne = "Monkey";
+            string PlayerTwo = "Monkey2";
+
+            int Monkey = rnd.Next(1, 7);
+            int Monkey2 = rnd.Next(1, 7);
+
+            Console.WriteLine($"{PlayerOne} veeretas {Monkey}");
+            Console.WriteLine($"{PlayerTwo} veeretas {Monkey2}");
+
+
+            if (Monkey < Monkey2)
+            {
+                Console.WriteLine($"{PlayerTwo} võitis numbriga {myRandomNumber}");
+            }
+            else if (Monkey > Monkey2)
+            {
+                Console.WriteLine($"{PlayerOne} võitis numbriga {myRandomNumber}");
+            }
+            else
+            {
+                Console.WriteLine("Viik!");
+            }
+                
+                    
+        }
+    }
+}
---
+++ RandomNumberGenerators/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace RandomNumberGenerators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int myRandomNumber = rnd.Next(1, 11);//number 1 kuni 10
+
+            Console.WriteLine($"Sinu juhuslik number on {myRandomNumber}");
+
+            //programm kontrollib, kas genereeritud number on suurem kui 5
+            //kui genereeritud number on suurem, kui 5, programm kuvab
+            //"juhuslik number on {myRandomNumber}, see on suurem kui 5
+            //kui genereeritud number on väiksem kui 5, siis programm kuvab
+            //juhuslik number on {myRandomNumber}, see on väiskem kui 5
+            //kui genereeritud number on 5, programm kuvab
+            //juhuslik number on 5
+            //
+
+            if (myRandomNumber < 5)
+            {
+                Console.WriteLine($"juhuslik number {myRandomNumber} on väiksem kui 5");
+            }               
+            else if (myRandomNumber > 5)
+            {
+                Console.WriteLine($"juhuslik number {myRandomNumber} on suurem kui 5");
+            }
+            else if (myRandomNumber == 5)
+            {
+                Console.WriteLine("juhuslik number on 5");
+            }    
+            
+        }
+    }
+}
